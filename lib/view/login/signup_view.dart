f34a826ff57466757bd22685b4b2b9e0941b2e63
Masthw <maverick17xd@gmail.com>
2023-11-21 09:53:40 -0300

    Login Page ui view

--- lib/view/login/signup_view.dart
+++ lib/view/login/signup_view.dart
@@ -1,3 +1,4 @@
+import 'package:fitness/common_widget/round_button.dart';
 import 'package:fitness/common_widget/round_textField.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -29,7 +30,7 @@ class _SignUpViewState extends State<SignUpView> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   SizedBox(
-                    height: media.height * 0.02,
+                    height: media.height * 0.01,
                   ),
                   Text(
                     "Hey there,",
@@ -86,6 +87,7 @@ class _SignUpViewState extends State<SignUpView> {
                             ))),
                   ),
                   Row(
+                    //crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       IconButton(
                           onPressed: () {
@@ -98,14 +100,120 @@ class _SignUpViewState extends State<SignUpView> {
                                 ? Icons.check_box_rounded
                                 : Icons.check_box_outline_blank_outlined,
                             color: TColor.gray,
+                            size: 20,
                           )),
-                      Expanded(
-                        child: Text(
-                          "Hey there,",
-                          style: TextStyle(color: TColor.gray, fontSize: 16),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Expanded(
+                          child: Text(
+                            "By continuing you accept our Privacy Policy and\n Terms of Use",
+                            style: TextStyle(color: TColor.gray, fontSize: 10),
+                          ),
                         ),
                       ),
                     ],
+                  ),
+                  SizedBox(
+                    height: media.height * 0.13,
+                  ),
+                  RoundButton(title: "Register", onPressed: () {}),
+                  SizedBox(
+                    height: media.height * 0.02,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            child: Container(
+                          height: 1,
+                          color: TColor.gray.withOpacity(0.5),
+                        )),
+                        Text(
+                          "     Or     ",
+                          style: TextStyle(
+                              color: TColor.black,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        Expanded(
+                            child: Container(
+                                height: 1,
+                                color: TColor.gray.withOpacity(0.5))),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: media.height * 0.01,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: TColor.white,
+                                border: Border.all(
+                                    width: 1,
+                                    color: TColor.gray.withOpacity(0.4)),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Image.asset(
+                              "assets/images/google-logo.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                          )),
+                      SizedBox(
+                        width: media.width * 0.1,
+                      ),
+                      GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: 50,
+                            height: 50,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                color: TColor.white,
+                                border: Border.all(
+                                    width: 1,
+                                    color: TColor.gray.withOpacity(0.4)),
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Image.asset(
+                              "assets/images/facebook_logo.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                          ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: media.height * 0.01,
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          "Already have an account? ",
+                          style: TextStyle(
+                            color: TColor.black,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                              color: TColor.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ],
+                    ),
                   ),
                 ]),
           ),
